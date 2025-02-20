9498a82c283a93732187b30c46859387dcdb9344
emiranivala <taitufam@gmail.com>
2025-02-20 19:40:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
-
-API_ID = int(getenv("API_ID", "22642292"))
-API_HASH = getenv("API_HASH", "4502d35191a2fcb02c8467f54789f0ea")
+      
+API_ID = int(getenv("API_ID", "24210243"))
+API_HASH = getenv("API_HASH", "509031fb3790b968e489f71d591ebce5")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002230414810")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002304203111"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "30"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000000000"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002458919549")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002294535138"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "20"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", None)
 AD_API = getenv("AD_API", None)
 STRING = getenv("STRING", None)
